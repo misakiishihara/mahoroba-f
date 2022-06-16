8013852ac2db17819f026d71a8daa49420a9076a
{自分の名前} <misaki.ishihara0004@gmail.com>
2022-06-16 15:14:15 +0900

    added a Rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -7,8 +7,7 @@ export default function Rightbar({ profile }) {
     
     const HomeRightbar = () => {
       return (
-        <div className='rightbar'>
-      <div className="rightbarWrapper">
+      <>
         <div className="eventContainer">
           <img src="assets/star.png" alt="" className='starImg' />
           <span className='eventText'><b>only followers event !</b></span>
@@ -38,18 +37,72 @@ export default function Rightbar({ profile }) {
         className='rightbarPromotionImg'
          />
          <p className='promotionName'>Shopping3</p>
-      </div>
-    </div>
+      </>
     )
+    }
+
     const ProfileRightbar = () => {
       return (
-        <></>
+        <>
+          <h4 className='rightbarTitle'>User Info</h4>
+          <div className="rightbarInfo">
+            <div className="rightbarInfoItem">
+              <sapn className="rightbarInfoKey">City:</sapn>
+              <sapn className="rightbarInfoKey">Tokyo</sapn>
+            </div>
+            <h4 className='rightbarTitle'>Friends</h4>
+            <div className="rightbarFollowings">
+              <div className="rightbarFollowing">
+                <img 
+                src="assets/person/1.jpeg"
+                alt="" 
+                className='rightbarFollowingImg'
+                />
+                <span className="rightbarFollowingName">Misaki</span>
+              </div>
+              <div className="rightbarFollowing">
+                <img 
+                src="assets/person/2.jpeg"
+                alt="" 
+                className='rightbarFollowingImg'
+                />
+                <span className="rightbarFollowingName">ブチャラティ</span>
+              </div>
+              <div className="rightbarFollowing">
+                <img 
+                src="assets/person/3.jpeg"
+                alt="" 
+                className='rightbarFollowingImg'
+                />
+                <span className="rightbarFollowingName">グイード・ミスタ</span>
+              </div>
+              <div className="rightbarFollowing">
+                <img 
+                src="assets/person/4.jpeg"
+                alt="" 
+                className='rightbarFollowingImg'
+                />
+                <span className="rightbarFollowingName">空条承太郎</span>
+              </div>
+              <div className="rightbarFollowing">
+                <img 
+                src="assets/person/5.jpeg"
+                alt="" 
+                className='rightbarFollowingImg'
+                />
+                <span className="rightbarFollowingName">ポルナレフ</span>
+              </div>
+            </div>
+          </div>
+        </>
       )
     }
-      }
+
       return (
-        <div></div>
-      
-      
+        <div className='rightbar'>
+          <div className="rightbarWrapper">
+            {profile ? <ProfileRightbar /> : <HomeRightbar />}
+          </div>
+        </div>
       )
 };
